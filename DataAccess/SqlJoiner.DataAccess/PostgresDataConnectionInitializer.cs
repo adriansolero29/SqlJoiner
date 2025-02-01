c2680960ff2d221a47a20cb7c7670479be211109
adriansolero <adrian.manlangit.solero@gmail.com>
2025-02-02 00:47:58 +0800

    Connection initializer

---
+++ DataAccess/SqlJoiner.DataAccess/PostgresDataConnectionInitializer.cs
@@ -0,0 +1,38 @@
+﻿using Npgsql;
+using SqlJoiner.Interfaces.DataAccess;
+using System.Text;
+
+namespace SqlJoiner.DataAccess
+{
+    public class PostgresDataConnectionInitializer : IDataConnectionInitializer
+    {
+        public void CloseConnection()
+        {
+            Connection.DbConnection?.Close();
+            Connection.DbConnection?.Dispose();
+        }
+
+        public async Task InitializeConnectionAsync()
+        {
+            await Task.Run(() =>
+            {
+                NpgsqlConnectionStringBuilder cnStringBuilder = new NpgsqlConnectionStringBuilder(Connection.ConnectionString);
+
+                if (Connection.DbConnection == null)
+                    Connection.DbConnection = new NpgsqlConnection(cnStringBuilder.ConnectionString);
+            });
+        }
+
+        public async Task OpenConnectionAsync()
+        {
+            await Task.Run(() =>
+            {
+                if (Connection.DbConnection == null)
+                    throw new ArgumentNullException("DbConnection is null");
+
+                if (Connection.DbConnection.State != System.Data.ConnectionState.Open)
+                    Connection.DbConnection.Open();
+            });
+        }
+    }
+}
---
+++ Interfaces/SqlJoiner.Interfaces/DataAccess/IDataConnectionInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlJoiner.Interfaces.DataAccess
+{
+    public interface IDataConnectionInitializer
+    {
+        Task InitializeConnectionAsync();
+        Task OpenConnectionAsync();
+        void CloseConnection();
+    }
+}
