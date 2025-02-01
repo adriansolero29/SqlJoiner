258b8035e3b11cc0052dd6bf22ff303f95bc967a
adriansolero <adrian.manlangit.solero@gmail.com>
2025-02-02 00:45:31 +0800

    for connection data

---
+++ DataAccess/SqlJoiner.DataAccess/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+
+namespace SqlJoiner.DataAccess
+{
+    public static class Connection
+    {
+        public static IDbConnection? DbConnection { get; set; }
+        public static string? ConnectionString
+        {
+            get
+            {
+                // set a custom path for your app.config
+                AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", "App.Config");
+                return ConfigurationManager.ConnectionStrings["postgres"]?.ConnectionString;
+            }
+        }
+    }
+}
