a6662370f35dee6e04b87a43d6a5dfcdf30ecb37
adriansolero <adrian.manlangit.solero@gmail.com>
2025-02-02 00:36:51 +0800

    Test project

---
+++ SqlJoiner.MsTest/TestConnection.cs
@@ -0,0 +1,23 @@
+﻿using SqlJoiner.DataAccess;
+
+namespace SqlJoiner.MsTest
+{
+    [TestClass]
+    public sealed class TestConnection
+    {
+        [TestMethod]
+        public async Task NpgsqlConnection()
+        {
+            PostgresDataConnectionInitializer p = new PostgresDataConnectionInitializer();
+
+            await p.InitializeConnectionAsync();
+            var a = Connection.DbConnection;
+
+            await p.OpenConnectionAsync();
+            var b = Connection.DbConnection;
+
+            p.CloseConnection();
+            var c = Connection.DbConnection;
+        }
+    }
+}
---
+++ SqlJoiner.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace SqlJoiner.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
