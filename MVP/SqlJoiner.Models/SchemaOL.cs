1bb363e4c7c46e887246ea91c7626c6a663e6e1e
adriansolero <adrian.manlangit.solero@gmail.com>
2025-03-29 00:13:07 +0800

    Property cleanup

--- MVP/SqlJoiner.Models/SchemaOL.cs
+++ MVP/SqlJoiner.Models/SchemaOL.cs
@@ -5,25 +5,11 @@ namespace SqlJoiner.Models
 	[Serializable]
     public class SchemaOL : IModel
     {
-		private string? _catalogName;
-		public string? CatalogName
-		{
-			get { return _catalogName; }
-			set { _catalogName = value; }
-		}
-
 		private string? _schemaName;
 		public string? SchemaName
 		{
 			get { return _schemaName; }
 			set { _schemaName = value; }
-		}
-
-		private string? _schemaOwner;
-		public string? SchemaOwner
-		{
-			get { return _schemaOwner; }
-			set { _schemaOwner = value; }
 		}
 	}
 }
