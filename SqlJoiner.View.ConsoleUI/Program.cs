448216d7cca3a87be1abdeb051b3c19a27c0811e
adriansolero <adrian.manlangit.solero@gmail.com>
2025-04-09 02:14:48 +0800

    Added UI for console. FOR TESTING

---
+++ SqlJoiner.View.ConsoleUI/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace SqlJoiner.View.ConsoleUI
+{
+    internal class Program
+    {
+        public Program()
+        {
+                
+        }
+
+        public async Task Start()
+        {
+            Console.WriteLine("===== SQL GENERATOR =====");
+            Console.Write("""
+                Menu:
+                1. Load Schemas 2. Exit
+
+                """);
+
+            string? menuResult = Console.ReadLine();
+
+            int selectedMenu = 0;
+            do
+            {
+                Console.Write("Invalid result. Please try again: ");
+                menuResult = Console.ReadLine();
+            }
+            while ((int.Parse(menuResult ?? "0") != 1 || (int.Parse(menuResult ?? "0") != 2) && int.TryParse(menuResult, out selectedMenu) == false));
+
+            Console.Clear();
+            Console.WriteLine("Loading Schemas...");
+
+
+
+
+            Console.ReadLine();
+        }
+
+        async static void Main(string[] args)
+        {
+            Program p = new Program();
+            await p.Start();
+        }
+    }
+}
